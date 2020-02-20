2d1dc6ac0d28d0d93423c61d1983158ef4b5c02d
maks1m_p <maksym.pohribniak@sixt.com>
2020-02-20 21:28:01 +0200

    add delete method

--- index.js
+++ index.js
@@ -7,18 +7,21 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const users = [
+let users = [
     {
         name: 'Alex',
         age: 20,
+        id: 1,
     },
     {
         name: 'Anna',
-        age: 40
+        age: 40,
+        id: 2
     },
     {
         name: 'Bakk',
         age: 32,
+        id: 3
     }
 ];
 
@@ -31,10 +34,21 @@ app.get('/users', (req, res) => {
 app.post('/users', (req, res) => {
     const {body: newUserData} = req;
 
-    users.push(newUserData);
+    users.push({
+        ...newUserData,
+        id: Date.now()
+    });
 
     res.status(201).send({status: 'OK'});
 });
+
+app.delete('/users/:id', (req, res) => {
+    const {id} = req.params;
+
+    users = users.filter(u => u.id !== parseInt(id));
+
+    res.status(200).send({status: 'OK'});
+})
 
 app.listen(3001, function () {
   console.log('Example app listening on port 3001!');
